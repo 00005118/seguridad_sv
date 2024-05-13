6f6772dd6ef56ddbd2c14286b35f16613de10a4d
Mburg03 <mburgosgit003@gmail.com>
2024-05-13 15:48:24 -0600

    Initial files added for UI

---
+++ Proyecto/UI/seguridad-UI/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga saepe minus obcaecati. Eligendi provident, tenetur debitis magni beatae tempora aliquam tempore omnis, quae inventore fugiat exercitationem eos sequi quis harum.</p>
+  )
+}
+
+export default App
