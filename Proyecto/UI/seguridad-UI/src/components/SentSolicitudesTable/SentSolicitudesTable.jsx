61fde80b20cdfd6cda702f3b4bd7193cd3c7679a
Mario <00063121@uca.edu.sv>
2024-05-18 23:16:45 -0600

    dinamyc table added, normal resident views done

---
+++ Proyecto/UI/seguridad-UI/src/components/SentSolicitudesTable/SentSolicitudesTable.jsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import dummyData from '../../data/SOLICITUDES.json';
+import './SentSolicitudesTable.css';
+import { useTable } from 'react-table';
+
+const SentSolicitudesTable = () => {
+    const data = React.useMemo(() => dummyData, []);
+    const columns = React.useMemo(() => [
+        {
+            Header: "Nombre",
+            accessor: "nombre-visita",
+        },
+        {
+            Header: "Fecha de visita",
+            accessor: "fecha-visita",
+        },
+        {
+            Header: "Hora de visita",
+            accessor: "hora-visita",
+        },
+        {
+            Header: "Estado",
+            accessor: "estado",
+        }
+    ], []);
+
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
+
+    return (
+        <div className='table'>
+            <div className="table-container">
+                <table {...getTableProps()}>
+                    <thead>
+                        {headerGroups.map(headerGroup => (
+                            <tr {...headerGroup.getHeaderGroupProps()}>
+                                {headerGroup.headers.map(column => (
+                                    <th {...column.getHeaderProps()}>
+                                        {column.render("Header")}
+                                    </th>
+                                ))}
+                            </tr>
+                        ))}
+                    </thead>
+                    <tbody {...getTableBodyProps()}>
+                        {rows.map(row => {
+                            prepareRow(row);
+                            return (
+                                <tr {...row.getRowProps()}>
+                                    {row.cells.map(cell => (
+                                        <td {...cell.getCellProps()}> {cell.render("Cell")} </td>
+                                    ))}
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+export default SentSolicitudesTable;
--- Proyecto/UI/seguridad-UI/src/pages/Resident/HistorialSolicitud.jsx
+++ Proyecto/UI/seguridad-UI/src/pages/Resident/HistorialSolicitud.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import NormalResidentHeader from '../../components/NormalResidentHeader/NormalResidentHeader'
+import SentSolicitudesTable from '../../components/SentSolicitudesTable/SentSolicitudesTable'
 
 const HistorialSolicitud = () => {
   return (
     <div>
       <NormalResidentHeader />
-      HistorialSolicitud
+      <div className='container'>
+          <SentSolicitudesTable />
+      </div>
     </div>
   )
 }
